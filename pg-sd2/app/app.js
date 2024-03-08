dc671cf6164a0568593ea896af2213b990b7e697
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-03-08 17:21:43 +0000

    new files

---
+++ pg-sd2/app/app.js
@@ -0,0 +1,64 @@
+// setting path
+const path = require('path')
+
+// Import express.js
+const express = require("express");
+
+// Create express app
+var app = express();
+
+// Add static files location
+app.use(express.static("static"));
+
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
+// Get the functions in the db.js file to use
+const db = require('./services/db');
+
+// public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Create a route for root - /
+app.get("/", function(req, res) {
+    res.send("Hello world!");
+});
+
+// Create a route for testing the db
+app.get("/db_test", function(req, res) {
+    // Assumes a table called test_table exists in your database
+    sql = 'select * from test_table';
+    db.query(sql).then(results => {
+        console.log(results);
+        res.send(results)
+    });
+});
+
+// Create a route for /goodbye
+// Responds to a 'GET' request
+app.get("/goodbye", function(req, res) {
+    res.send("Goodbye world!");
+});
+
+app.get("/landing_page", function(req, res) {
+    res.render('landing_page', {
+        title: 'Landing Page',
+    });
+});
+
+// Create a dynamic route for /hello/<name>, where name is any value provided by user
+// At the end of the URL
+// Responds to a 'GET' request
+app.get("/hello/:name", function(req, res) {
+    // req.params contains any parameters in the request
+    // We can examine it in the console for debugging purposes
+    console.log(req.params);
+    //  Retrieve the 'name' parameter and use it in a dynamically generated page
+    res.send("Hello " + req.params.name);
+});
+
+// Start server on port 3000
+app.listen(3000,function(){
+    console.log(`Server running at http://127.0.0.1:3000/`);
+});
