c9cb53165b80066adbe52a450063f36d2a1df464
NourHRadwan <106489456+NourHRadwan@users.noreply.github.com>
2024-03-18 17:10:54 +0000

    design

--- app/app.js
+++ app/app.js
@@ -6,11 +6,8 @@ const categoryController = require('./controllers/categoryController');
 const transactionController = require('./controllers/transactionController');
 const { getCategories } = require('./models/categoryModel');
 const transactionModel = require('./models/transactionModel');
-//const morgan = require('morgan')
 
 const app = express();
-
-//app.use(morgan("tiny"))
 
 app.use(express.static('static'));
 app.set('view engine', 'pug');
