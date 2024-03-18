6abcd1931c89ebe780d6daea49d94628914371fc
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-03-18 18:56:41 +0000

    fixed the app.js

--- app/app.js
+++ app/app.js
@@ -1,25 +1,32 @@
-const express = require('express');
-const path = require('path');
-const createError = require('http-errors');
-const userController = require('./controllers/userController');
-const categoryController = require('./controllers/categoryController');
-const transactionController = require('./controllers/transactionController');
-const { getCategories } = require('./models/categoryModel');
-const transactionModel = require('./models/transactionModel');
+const express = require("express");
+const path = require("path");
+const createError = require("http-errors");
+
+const userController = require("./controllers/userController");
+const categoryController = require("./controllers/categoryController");
+const transactionController = require("./controllers/transactionController");
+const signupandloginController = require("./controllers/signupandloginController");
 
 const app = express();
 
-app.use(express.static('static'));
-app.set('view engine', 'pug');
-app.set('views', './app/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("static"));
+app.set("view engine", "pug");
+app.set("views", "./app/views");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/users', userController);
-app.use('/categories', categoryController);
-app.use('/transactions', transactionController);
+app.use("/users", userController);
+app.use("/categories", categoryController);
+app.use("/transactions", transactionController);
+app.use("/auth", signupandloginController);
+
+app.get("/landing_page", (req, res) => {
+  res.render("landing_page");
+});
 
 app.get('/', (req, res) => {
-  res.redirect('/home')
+  res.redirect('/landing_page')
 });
 
 app.get('/home', async (req, res, next)=> {
@@ -33,15 +40,19 @@ app.get('/home', async (req, res, next)=> {
   })
 })
 
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-  console.log(`http://localhost:${port}`)
+  console.log(`http://localhost:${port}`);
 });
